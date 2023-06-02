100ea320bb9b2258004ce2f9ea609ef59cd7ec06
Martin Masson <martin.masson@datastorm.fr>
2023-06-02 15:55:24 +0200

    FEAT: Add an image created from a reactive

--- server.R
+++ server.R
@@ -19,10 +19,12 @@ function(input, output, session) {
       bins <- seq(min(x), max(x), length.out = input$bins + 1)
       
       # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white',
-           xlab = 'Waiting time to next eruption (in mins)',
-           main = 'Histogram of waiting times')
+      h <- hist(
+        x, breaks = bins, col = 'darkgray', border = 'white',
+         xlab = 'Waiting time to next eruption (in mins)',
+         main = 'Histogram of waiting times')
       
+      return(h)
     })
   
     output$distPlot <- renderPlot({my_plot()})
@@ -37,20 +39,31 @@ function(input, output, session) {
         options(envelope.details = TRUE)
         
         email <- emayili::envelope()
-        
         email <- email %>%
           to("bob@google.com") %>%
-          cc("craig@google.com")
+          cc("craig@google.com") %>% 
+          subject("Créer automatiquement des emails de rapport depuis Shiny!")
         
-        email <- email %>% subject("Créer automatiquement des emails de rapport depuis Shiny!")
+        # Prepare image and attach image
+        png("my_plot.png")
+        plot(my_plot())
+        dev.off()
+        img_base64 <- base64enc::base64encode("my_plot.png")
+        #email <- email %>% attachment(path = "my_plot.png", cid = "my_plot")
         
+        # Write email body
         email <- email %>% html(
           tagList(
             h2("Hello"),
-            p("World!")
+            p("World!"),
+            img(src=sprintf("data:image/png;base64,%s", img_base64), width='200px', height='180px'),
+            #img(src='cid:my_plot', width='50px', height='50px')
           )
         )
         
+        # Write EML file
+        # writeLines instead of cat to ensure consistent CRLF
+        # Thanks to X-Unset, Outlook will open eml file as draft
         eml <- as.character(email)
         eml <- strsplit(eml, "\\r\\n")
         writeLines(c("X-Unsent: 1", eml[[1]]), con = file)
